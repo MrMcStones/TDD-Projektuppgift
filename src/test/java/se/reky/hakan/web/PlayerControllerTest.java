1b7fcf1adeb1a911f603c1d65c89048dbbf84ae2
MrMcStones <rasmus.ke.nilsson@gmail.com>
2024-05-17 15:41:42 +0200

    Comments added PlayerControllerTest

--- src/test/java/se/reky/hakan/web/PlayerControllerTest.java
+++ src/test/java/se/reky/hakan/web/PlayerControllerTest.java
@@ -12,25 +12,30 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled
+
+@Disabled // Klasser körs ej när applikation byggs
 public class PlayerControllerTest {
 
-    private WebDriver driver;
-    private final String url = "http://localhost:8080/";
+    private WebDriver driver; // WebDriver-instans, kontrollera webbläsare
+    private final String url = "http://localhost:8080/"; // Bas-URL för applikation
+
 
     @BeforeAll
     public static void setupClass() {
+        // Ställer in WebDriverManager för att hantera ChromeDriver
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setupTest() {
+        // Skapar instans av ChromeDriver till /players endpoint
         driver = new ChromeDriver();
         driver.get(url + "players");
     }
 
     @AfterEach
     public void tearDown() {
+        // Avslutar WebDriver efter varje test
         if (driver != null) {
             driver.quit();
         }
@@ -38,23 +43,29 @@ public class PlayerControllerTest {
 
     @Test
     public void testPlayerCount() {
+        // Hittar spelare i lista genom <li>-element
         List<WebElement> players = driver.findElements(By.tagName("li"));
+        // Verifierar att antal spelare är 2 i lista
         assertEquals(2, players.size());
     }
 
     @Test
-    public void testFirstPlayerNameDisplayed() {
+    public void testFirstPlayerNameIsDisplayed() {
+        // Hittar det första <li>-elementet
         WebElement firstPlayerName = driver.findElement(By.tagName("li"));
+        // Verifierar att första namn i lista visas
         assertTrue(firstPlayerName.isDisplayed());
     }
 
     @Test
     public void testPageTitle() {
+        // Verifierar att titelsida är "Players List"
         assertEquals("Players List", driver.getTitle());
     }
 
     @Test
     public void testLoginButtonText() {
+        // Hittar button och verifierar att text är "Logga in"
         assertEquals("Logga in", driver.findElement(By.tagName("button")).getText());
     }
 }
